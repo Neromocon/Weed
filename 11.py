3ab5b6afa903965f348ae198135927932c40c9b0
Neromocon <164160223+Neromocon@users.noreply.github.com>
2024-03-20 18:36:35 +0900

    Add files via upload

---
+++ 11.py
@@ -0,0 +1,22 @@
+#print(" Subject : Python Language", end="\n\n")
+#print("*"*20)
+
+
+#a = 128
+#print("***number***")
+#print("10진수 일때 : ", format(a, 'd'))
+
+#print("10진수 일때 : {0:d}" .format(a))
+
+
+x =  1000000
+
+y = x / 1236.80
+
+#print("1,000,000 KRW →", format(y, "8.2f") "USD")
+#print("1,000,000 KRW →", f"{y: .2f}", "USD")
+
+a = 123
+b = 2/3
+print(format("a:{a} b:{b}"))
+print(f"a:{a:012d} b:{b:.2f}")
