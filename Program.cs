7041ee8c73eebaf3f15c26bd17aceaf20bc190ce
Neromocon <164160223+Neromocon@users.noreply.github.com>
2024-06-12 18:59:03 +0900

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,107 +4,169 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace _24._06._11
+namespace _24
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            //string rsp = "2052205";
-            //string answer = "";
+            //int n = 3;
+            //int[] numlist = new int[] { 4, 5, 6, 7, 8, 9, 10, 11, 12 };
+            //List<int> list = new List<int>();
 
-
-            //foreach (char c in rsp) 
+            //for (int i = 0; i < numlist.Length; i++) 
             //{
-            //    if (c == '2')
-            //    {
-            //        answer += "0";
-            //    }
-            //    else if (c == '0')
-            //    {
-            //        answer += "5";
-            //    }
-            //    else if (c == '5')
-            //    {
-            //        answer += "2" ;
-            //    }
+            //    if (numlist[i] % n == 0)
+            //        list.Add(i);
             //}
+            //foreach (int num in numlist)
+            //{
+            //    if (num % n == 0)
+            //        list.Add(num);
+            //}
+
+            //int[] answer = list.ToArray();
+            //foreach (int i in answer) 
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+            //int answer = 0;
+            //int a = 2;
+            //int b = 4;
+            //if (a % 2 != 0 && b % 2 != 0) 
+            //{
+            //    answer = (a*a) + (b*b);
+            //}
+            //else if(a % 2 != 0 || b % 2 != 0)
+            //{
+            //    answer = 2 * (a + b);
+            //}
+            //else if(a % 2 == 0 && b % 2 == 0)
+            //{
+            //    answer = Math.Abs(a - b);
+            //}
+
             //Console.WriteLine(answer);
 
-            //string my_string = "1a2b3c4d123";
+            //string[] str_list = { "abc", "def", "ghi" };
+            //string ex = "ef";
+            ////string[] str_list = { "abc", "bbc", "cbc" };
+            ////string ex = "c";
+            //string answer = "";
+
+            //List<string> list = new List<string>();
+            //foreach (string str in str_list) 
+            //{
+            //    if(!str.Contains(ex))
+            //    {
+            //        list.Add(str);
+            //    }
+
+            //}
+
+            //Console.WriteLine(list.ToArray());
+
+            //int[] num_list = { 1, 2, 3, 4, 5 };
+            //int n = 6;
             //int answer = 0;
 
-            //foreach (char c in my_string) 
+
+            //foreach (int i in num_list) 
             //{
-            //    if (char.IsDigit(c))
-            //        answer += (c - 48);
+            //    if (i == n)
+            //    {
+            //        answer = 1;
+            //        break;
+            //    }
+
             //}
             //Console.WriteLine(answer);
 
-            //string cipher = "pfqallllabwaoclk";
-            //int code = 2;
+            //string str1 = "abc";
+            //string str2 = "aabcc";
+
+            //bool isContained = str2.Contains(str1);
+
+            //Console.WriteLine(isContained ? 1 : 2);
+
+            //string n_str = "854020";
             //string answer = "";
 
-            //for (int i = 0; i < cipher.Length; i++)
-            //{
-            //    if ((i + 1) % code == 0)
-            //        answer += cipher[i].ToString();
-            //}
+            //bool findZero = false;
 
-            //int x = 0;
-
-            //foreach (char c in cipher) 
+            //foreach (char c in n_str) 
             //{
-            //    if((x+1) % code == 0)
+            //    if(c != '0' || findZero)
+            //    {
             //        answer += c;
-            //    x++;
+            //        findZero = true;
+            //    }
+
             //}
             //Console.WriteLine(answer);
 
-            //string my_string = "nice to meet you";
-            //string answer = "";
-            //foreach (char c in my_string) 
+            //string[] strArr = {"and","notad","abcd" };
+            //string[] answer = new string[] { };
+            //string str = "ad";
+
+            //List<string> list = new List<string>();
+            //foreach (string ex in strArr)
             //{
-            //    if(c != 'a' && c != 'e' && c != 'i' && c != 'o' && c != 'u')
-            //        answer += c;
+            //    if (!ex.Contains(str))
+            //    {
+            //        list.Add(ex);
+            //    }
+
             //}
-            //Console.WriteLine(answer);
+            //answer = list.ToArray();
 
-            //string message = "I love you~";
-            //int len = 2;
-            //int answer = message.Length * len;
+            //int[] arr = { 1, 2, 3, 100, 99, 98 };
+            //int k = 2;
+            //int[] answer = new int[arr.Length];
 
-            //Console.WriteLine(answer);
-
-            //string my_string = "abCdEfghIJ";
-
-            //char[] c = my_string.ToCharArray();
-            //for(int i = 0; i < my_string.Length; i++)
+            //for(int i = 0; i < arr.Length; i++) 
             //{
-            //    if (char.IsUpper(c[i]))
-            //        c[i] = char.ToLower(c[i]);
-            //    else if (char.IsLower(c[i]))
-            //        c[i] = char.ToUpper(c[i]);
+            //    if (k % 2 != 0)
+            //    {
+            //        answer[i] += arr[i] * k;
+            //    }
+            //    else 
+            //    {
+            //        answer[i] += arr[i] + k;
+            //    }
             //}
-            //string answer = new string(c);
-            //Console.WriteLine(answer);
+            //Console.WriteLine("[" + string.Join(", ", answer) + "]");
 
-            int[] array = { 1, 8, 3 };
-            int max = int.MinValue;
-            int maxIndex = -1;
+            //int[] num_list = { 12, 4, 15, 46, 38, 1, 14, 56, 32, 10 };
+            //Array.Sort(num_list);
 
-            
-            for (int i = 0; i < array.Length; i++) 
+            //int[] answer = num_list.Skip(5).ToArray();
+            //Array.Sort(answer);
+
+            int[] arr = { 49, 12, 100, 276, 33 };
+            int n = 27;
+            int[] answer = new int[arr.Length];
+
+            for (int i = 0; i < arr.Length; i++) 
             {
-                if (array[i] > max)
+                if (arr.Length % 2 != 0)
                 {
-                    max = array[i];
-                    maxIndex = i;
-                } 
-               
+                    for (int j = 0; j < arr.Length; i = +2, j++)
+                    {
+                        answer[i] = arr[j] + n;
+                    }
+                }
+                else 
+                {
+                    for (int j = 1; j < arr.Length; i = +2, j++)
+                    {
+                        answer[i] = arr[j] + n;
+                    }
+                }
             }
-            int[] answer = new int[] { max, maxIndex};
-            Console.WriteLine(answer);
+
+
 
         }
     }
